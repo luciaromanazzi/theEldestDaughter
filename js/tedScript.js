4ae9c8743a8ba0c6e5d88c7ed44aae360ea672ed
luciaromanazzi <romanazzi.lu@gmail.com>
2026-07-09 14:59:44 +0200

    Articles view and preview

--- js/tedScript.js
+++ js/tedScript.js
@@ -1,72 +1,164 @@
-console.log("START TED SCRIPT");
-alert("JS caricato correttamente");
-console.log("TED SCRIPT CARICATO");
-fetch("data/articoli.json")
-.then(response => response.json())
-.then(articles => {
+document.addEventListener("DOMContentLoaded", () => {
+    loadArticles();
+    loadPreview();
+    privacySetup();
+});
+function loadArticles(){
+    fetch("data/articoli.json")
+    .then(response => response.json())
+    .then(articles => {
+        articles = sortArticles(articles);
+        const container = document.getElementById("blog-feed");
+        const limit = container.dataset.limit;
+        if (limit){
 
-    const container = document.getElementById("blog-feed");
-    articles.sort((a, b) => b.ID - a.ID);
-    articles.forEach(article => {
+            articles = articles.slice(0,limit);
+        }
+        articles.forEach(article =>{
+            renderArticle(article,container);
+        });
 
-        const articleElement = document.createElement("article");
+    })
 
-        articleElement.classList.add("blog-card");
+    .catch(error => {
+        console.error("Errore caricamento articoli:", error);
+    });
+}
+function loadPreview(limit){
+    fetch("data/articoli.json")
+    .then(response => response.json())
+    .then(articles => {
+        articles = sortArticles(articles);
+        const container = document.getElementById("blog-preview");
+        const limit = container.dataset.limit;
+        if (limit){
 
-        articleElement.innerHTML = `
+            articles = articles.slice(0,limit);
+        }
+        articles.forEach((article, index) =>{
+            if (index === articles.length - 1) {
+                // ultima iterazione, passo index per rimuovere linea divisoria
+                renderPreview(article,container, index);
+            }else{
+                renderPreview(article,container);
+            }
+            
+            
+        });
 
-        <article class="article-card">
+    })
 
-            <img 
-                src="${article.cover}" 
-                alt="${article.title}"
-                class="article-image"
-            >
+    .catch(error => {
+        console.error("Errore caricamento articoli:", error);
+    });
+}
+function sortArticles(articles) {
+    return articles.sort((a, b) => b.ID - a.ID);
+}
+function renderArticle(article, container){
+    const articleElement = document.createElement("article");
 
-            <div class="article-body">
+    articleElement.classList.add("blog-card");
 
-                <span class="article-category">
-                    ${article.date} - 
+    articleElement.innerHTML = `
+
+    <article class="article-card">
+
+        
+
+        <div class="article-body">
+
+            <span class="article-category">
+                ${article.date} - 
+            </span>
+            <span class="article-author">
+                @ ${article.author}
+            </span><br>
+            <span class="article-category">
+                ${article.tag}
+            </span>
+
+
+            <h2>
+                ${article.title}
+            </h2>
+
+
+            <p class="article-preview">
+                ${article.preview}
+            </p>
+
+
+            <button class="read-more">
+                Read more
+            </button>
+
+
+            <div class="article-content hidden">
+                ${article.paragraph}
+            </div>
+
+        </div>
+
+    </article>
+
+    `;
+
+    container.appendChild(articleElement);
+}
+function renderPreview(article,container, index = null){
+    const articleElement = document.createElement("article");
+    
+    articleElement.classList.add("blog-card");
+    const breakline = "<hr>"
+    articleElement.innerHTML = `
+    <a href="${article.ID}.html">
+        <article class="preview-card">
+            <div class="preview-body">
+                <span class="preview-category">
+                    ${article.date}
                 </span>
-                <span class="article-author">
-                    @ ${article.author}
-                </span><br>
-                <span class="article-category">
-                    ${article.tag}
-                </span>
-
-
                 <h2>
                     ${article.title}
                 </h2>
-
-
-                <p class="article-preview">
-                    ${article.preview}
-                </p>
-
-
-                <button class="read-more">
-                    Read more
+                <button class="read">
+                    Read 
                 </button>
-
-
-                <div class="article-content hidden">
-                    ${article.paragraph}
-                </div>
-
             </div>
-
         </article>
+    </a>
+    
+    `;
+    if (!index){
+        articleElement.innerHTML = articleElement.innerHTML+breakline;
+    }else{
+        articleElement.innerHTML = articleElement.innerHTML;
+    }
+    container.appendChild(articleElement);
+}
+/////////////////////////////////////////////////// UTILITIES /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function privacySetup(){
+    const privacyLink = document.querySelector("#privacy-link");
+    const privacyModal = document.querySelector("#privacy-modal");
+    const closePrivacy = document.querySelector("#close-privacy");
+    if (privacyLink && privacyModal && closePrivacy) {
 
-        `;
 
-        container.appendChild(articleElement);
+        privacyLink.addEventListener("click", (event) => {
+            event.preventDefault();
+            privacyModal.style.display = "flex";
+        });
 
-    });
 
-})
+        closePrivacy.addEventListener("click", () => {
+            privacyModal.style.display = "none";
+        });
 
-.catch(error => {
-    console.error("Errore caricamento articoli:", error);
-});
+
+        privacyModal.addEventListener("click", (event) => {
+            if (event.target === privacyModal) {
+                privacyModal.style.display = "none";
+            }
+        });
+    }
+}
